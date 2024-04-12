f533560376d425b522c710a3aaa15c030134d148
Abdul Karim Tamim <abdultmim@gmail.com>
2024-04-11 18:31:13 -0700

    Designed and implemented the website's footer in html, css, and javascript

---
+++ Website Footer/footer.js
@@ -0,0 +1,5 @@
+// Get the current year
+var currentYear = new Date().getFullYear();
+
+// Update the content of the currentYear span element
+document.getElementById("currentYear").textContent = currentYear;
