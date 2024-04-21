6a5ba81d192b7a172db79e4c4357e2894bcbd57e
EthanT <ethanmtift@gmail.com>
2024-04-21 15:33:44 -0700

    Backend code exchanging data, stuff in my version of employment.jsx

--- backend/gaming/models.py
+++ backend/gaming/models.py
@@ -1,12 +1,13 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
 
 class gaming(models.Model):
     Name = models.CharField(max_length=50)
-    redID = models.IntegerField(max_length=10)
+    redID = models.IntegerField()
     device = models.CharField(max_length=20)
-    time = models.DateField()
+    time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Gaming: {self.Name}"
